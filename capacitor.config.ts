3ff9d1994bcafbb845f62d9963bded22dd1ab48a
alicarlo <alicarlo.montijop@gmail.com>
2025-02-24 09:46:18 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'patoZambranoApp',
+  webDir: 'www'
+};
+
+export default config;
